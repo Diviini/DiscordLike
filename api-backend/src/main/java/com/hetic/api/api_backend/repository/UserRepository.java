eb33c3269f6853257198dab8f45d7cbc58e2a4c8
Robin LE BROZEC <robin.lebrozec@gmail.com>
2025-04-01 11:10:49 +0200

    add Auth

---
+++ api-backend/src/main/java/com/hetic/api/api_backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.hetic.api.api_backend.repository;
+
+import com.hetic.api.api_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
